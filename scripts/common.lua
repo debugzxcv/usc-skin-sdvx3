ddccca310ceade4bf73fb3395e891de8abcd9681
Koji AGAWA <atty303@gmail.com>
2019-04-14 17:25:32 +0900

    wip

--- scripts/common.lua
+++ scripts/common.lua
@@ -1,6 +1,7 @@
 gfx.LoadSkinFont("segoeui.ttf")
 
 -- Image class
+--------------
 Image = {}
 Image.skin = function(filename, imageFlags)
   local this = {
@@ -23,3 +24,53 @@ Image.drawSize = function(this, x, y, w, h, alpha, angle)
   gfx.BeginPath()
   gfx.ImageRect(x - w / 2, y - h / 2, w, h, this.image, alpha, angle)
 end
+
+-- ImageFont class
+------------------
+ImageFont = {}
+ImageFont.new = function(path, chars)
+  local this = {
+    images = {}
+  }
+  -- load character images
+  for i = 1, chars:len() do
+    local c = chars:sub(i, i)
+    local image = Image.skin(string.format("%s/%s.png", path, c), 0)
+    this.images[c] = image
+  end
+  -- use size of first char
+  local w, h = gfx.ImageSize(this.images[chars:sub(1, 1)].image)
+  this.w = w
+  this.h = h
+
+  setmetatable(this, {__index = ImageFont})
+  return this
+end
+ImageFont.draw = function(this, text, x, y, alpha, textFlags)
+  local totalW = text:len() * this.w
+
+  -- adjust horizontal alignment
+  if textFlags and gfx.TEXT_ALIGN_CENTER then
+    x = x - totalW / 2
+  elseif textFlags and gfx.TEXT_ALIGN_RIGHT then
+    x = x - totalW
+  end
+
+  -- adjust vertical alignment
+  if textFlags and gfx.TEXT_ALIGN_MIDDLE then
+    y = y - this.h / 2
+  elseif textFlags and gfx.TEXT_ALIGN_BOTTOM then
+    y = y - this.h
+  end
+
+  for i = 1, text:len() do
+    local c = text:sub(i, i)
+    local image = this.images[c]
+    if image ~= nil then
+      gfx.BeginPath()
+      gfx.ImageRect(x, y, this.w, this.h, image.image, alpha, 0)
+      x = x + this.w
+    end
+  end
+end
+
