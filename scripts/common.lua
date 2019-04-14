6d8fc65eaebf78023c3a1108d84dfd47911fe9da
Koji AGAWA <atty303@gmail.com>
2019-04-14 15:00:51 +0900

    wip song

--- scripts/common.lua
+++ scripts/common.lua
@@ -1,1 +1,25 @@
-gfx.LoadSkinFont("segoeui.ttf");
+gfx.LoadSkinFont("segoeui.ttf")
+
+-- Image class
+Image = {}
+Image.skin = function(filename, imageFlags)
+  local this = {
+    image = gfx.CreateSkinImage(filename, imageFlags)
+  }
+  local w, h = gfx.ImageSize(this.image)
+  this.w = w
+  this.h = h
+  setmetatable(this, {__index = Image})
+  return this
+end
+-- anchor point is center
+Image.draw = function(this, x, y, alpha, angle)
+--  gfx.BeginPath()
+--  gfx.ImageRect(x - this.w / 2, y - this.h / 2, this.w, this.h, this.image, alpha, angle)
+  this:drawSize(x, y, this.w, this.h, alpha, angle)
+end
+
+Image.drawSize = function(this, x, y, w, h, alpha, angle)
+  gfx.BeginPath()
+  gfx.ImageRect(x - w / 2, y - h / 2, w, h, this.image, alpha, angle)
+end
--- scripts/songselect/songwheel.lua
+++ scripts/songselect/songwheel.lua
@@ -1,14 +1,127 @@
-local wheelSize = 12
+require("easing")
 
+gfx.LoadSkinFont("UDDigiKyokashoNP-B.ttf")
+gfx.LoadSkinFont("rounded-mplus-1c-bold.ttf")
+
+game.LoadSkinSample("cursor_song")
+
+
+
+-- SongTable class
+SongTable = {}
+SongTable.new = function()
+  local this = {
+    cols = 3,
+    rows = 3,
+    selectedIndex = 1,
+    selectedDifficulty = 0,
+    cache = {},
+    images = {
+      jacketLoading = Image.skin("song_select/jacket_loading.png", 0),
+      cursor = Image.skin("song_select/cursor.png", 0),
+      plates = {
+        Image.skin("song_select/plate/novice.png", 0),
+        Image.skin("song_select/plate/advanced.png", 0),
+        Image.skin("song_select/plate/exhaust.png", 0),
+        Image.skin("song_select/plate/gravity.png", 0)
+      }
+    }
+  }
+  setmetatable(this, {__index = SongTable})
+  return this
+end
+
+SongTable.set_index = function(this, newIndex)
+  if newIndex ~= this.selectedIndex then
+    game.PlaySample("cursor_song")
+  end
+  this.selectedIndex = newIndex
+end
+
+SongTable.render = function(this, deltaTime)
+  for i = 1, #songwheel.songs do
+    this:render_song(i)
+  end
+  this:render_cursor()
+end
+
+-- Draw the song plate
+SongTable.render_song = function(this, index)
+  local song = songwheel.songs[index]
+
+  -- Initialize song cache
+  if not this.cache[song.id] then
+    this.cache[song.id] = {}
+    this.cache[song.id]["jacket"] = {}
+  end
+
+  -- Lookup difficulty
+  local diffIndex = 1
+  for i, v in ipairs(song.difficulties) do
+    if v.difficulty == this.selectedDifficulty then
+      diffIndex = i
+    end
+  end
+  local diff = song.difficulties[diffIndex]
+
+  local col = (index - 1) % this.cols
+  local row = math.floor((index - 1) / this.cols)
+  local x = 154 + col * this.images.cursor.w + 4
+  local y = 478 + row * this.images.cursor.h + 16
+
+  -- Draw the background (plate)
+  gfx.FillColor(255, 255, 255)
+  this.images.plates[diff.difficulty + 1]:draw(x, y, 1, 0)
+
+  -- Draw the jacket
+  local jacket = this.cache[song.id]["jacket"][diff.id]
+  if not jacket or jacket == this.images.jacketLoading.image then
+    jacket = gfx.LoadImageJob(diff.jacketPath, this.images.jacketLoading.image)
+    this.cache[song.id]["jacket"][diff.id] = jacket
+  end
+  local jacketSize = 122
+  gfx.FillColor(255, 255, 255, 1)
+  gfx.BeginPath()
+  gfx.ImageRect(x - 24 - jacketSize / 2, y - 21 - jacketSize / 2, jacketSize, jacketSize, jacket, 1, 0)
+
+  -- Draw the title
+  local title = this.cache[song.id]["title"]
+  if not title then
+    -- gfx.FontFace("UDDigiKyokashoNP-B.ttf")
+    gfx.FontFace("rounded-mplus-1c-bold.ttf")
+    gfx.TextAlign(gfx.TEXT_ALIGN_CENTER or gfx.TEXT_ALIGN_BASELINE)
+    title = gfx.CreateLabel(song.title, 14, 0)
+    this.cache[song.id]["title"] = title
+  end
+  gfx.DrawLabel(title, x - 22, y + 63, 125)
+end
+
+SongTable.render_cursor = function(this)
+  local col = (this.selectedIndex - 1) % this.cols
+  local row = math.floor((this.selectedIndex - 1) / this.cols)
+  local x = 154 + col * this.images.cursor.w
+  local y = 478 + row * this.images.cursor.h
+  gfx.FillColor(255, 255, 255)
+  this.images.cursor:draw(x, y, 1, 0)
+end
+
+local wheelSize = 12
 get_page_size = function()
     return wheelSize
 end
 
+songTable = SongTable.new()
+
+-- Callback
 render = function(deltaTime)
+  songTable:render(deltaTime)
 end
 
+-- Callback
 set_index = function(newIndex)
+  songTable:set_index(newIndex)
 end
 
+-- Callback
 set_diff = function(newDiff)
 end
